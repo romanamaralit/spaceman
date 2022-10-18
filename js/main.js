45fc08f06c439e263954e4ca5529c2d6f534f7d6
romanamaralit <romana.maralit@gmail.com>
2022-10-17 20:31:49 -0700

    Let word that user input be displayed on the screen

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
+//Constants
 const startButton=document.querySelector("#wordBoxButton")
 const wordBox=document.querySelector("#wordToGuessBox")
+const wordInput=document.querySelector("input")
+const letters = document.querySelectorAll(".grayletter")
+const spaceship = document.querySelectorAll(".images")
+const alien = document.querySelector("#alien")
+const ring = document.querySelector("#ring")
+const engine1 = document.querySelector("#engine1")
+const engine2 = document.querySelector("#engine2")
+const engine3 = document.querySelector("#engine")
+const dome = document.querySelector("#dome")
+const window1 = document.querySelector("#window1")
+const window2 = document.querySelector("#window2")
+const window3 = document.querySelector("#window3")
+const antenna = document.querySelector("#antenna")
+const buildButton = document.querySelector(".buildButton")
+// const playAgainButton = play again button
 
-startButton.addEventListener("click", function (event) {
+console.log(letters)
+
+//Event Listeners
+// playAgainButton.addEventListener
+startButton.addEventListener("click", startGame)
+buildButton.addEventListener("click", buildButton)
+
+
+
+//Functions
+function startGame(startGame) {
     const word = document.createElement("wordToGuessBox")
-    const teacherword = document.querySelector("input")
-    console.log(teacherword.value)
-
+    const teacherword = wordInput
     word.innerText=teacherword.value
     wordBox.appendChild(word)
     teacherword.value=""
-})
+
+   
+
+    }
